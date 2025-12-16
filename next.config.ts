110bb49e38270b9b38a8b1defd61fb5639f46056
Mukul Chauhan <mc.mukulchauhan.abc@gmail.com>
2025-12-16 15:02:21 +0530

    basePath Update Patch

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const isProd = process.env.NODE_ENV === 'production'
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  basePath: isProd ? "rsx-writeups" : ""
+  basePath: isProd ? "/rsx-writeups" : ""
 };
 
 export default nextConfig;
