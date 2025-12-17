33afad4daf286b0658d510e710db6256064626e8
Mukul Chauhan <mc.mukulchauhan.abc@gmail.com>
2025-12-17 15:19:43 +0530

    latest commit

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,8 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production'
-
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  basePath: isProd ? "/rsx-writeups" : ""
 };
 
 export default nextConfig;
