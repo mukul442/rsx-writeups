9f0a4ac7269378cb694f15e20c60d6a52e5efb96
Mukul Chauhan <mc.mukulchauhan.abc@gmail.com>
2025-12-22 12:51:48 +0530

    init 1.1.3

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,6 +1,6 @@
 export default async function Page({params,}: {params: Promise<{slug: string}>}){
     const { slug } = await params
-    const { default: Post } = await import(`@/_contents/${slug}.mdx`)
+    const { default: Post } = await import(`@/_contents/${slug}.md`)
 
     return <Post/>
 }
