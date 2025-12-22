ac11ad1bca6dfa748826f35b1a78ca9ae4087772
Mukul Chauhan <mc.mukulchauhan.abc@gmail.com>
2025-12-22 12:11:53 +0530

    init 1.1.1

---
+++ app/imp-mdx/page.tsx
@@ -0,0 +1,13 @@
+import Welcome from "@/markdowns/welcome.mdx";
+
+function CustomH1({ children }: {children: any}) {
+  return <h1 style={{ color: 'blue', fontSize: '100px' }}>{children}</h1>
+}
+
+const overrideComponents = {
+  h1: CustomH1,
+}
+
+export default function Page(){
+    return <Welcome components={overrideComponents} />;
+}
--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,17 @@
 import type { NextConfig } from "next";
+import createMDX from '@next/mdx' 
 
 const nextConfig: NextConfig = {
   /* config options here */
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   output: 'export',
   images: {
     unoptimized: true
   }
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+})
+
+export default withMDX(nextConfig);
